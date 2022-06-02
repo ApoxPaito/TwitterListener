d622c2cdea7f6e91a50cbd5f680af4f88193135a
ayhanalpertokgoz <ayhanalpertokgoz@hotmail.com>
2022-06-02 09:39:55 +0300

    Seasonal XPath change I guess, Twitter likes playing with their HTML every now and then

--- TwitterListener/Listener.cs
+++ TwitterListener/Listener.cs
@@ -7,10 +7,11 @@ namespace TwitterListener
     static class Listener
     {
         private const string relativeFirst = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div" +
-            "/div[1]/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]"; // This is the XPath of first relative Tweet in the feed
+            "/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]"; // This is the XPath of first relative Tweet in the feed
         private const string relativeSecond = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div" +
-            "/div[2]/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]"; // This is the XPath of second relative Tweet in the feed
-        private const string relativeThird = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[3]/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]"; // This is the XPath of third relative Tweet in the feed
+            "/div[2]/div/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]"; // This is the XPath of second relative Tweet in the feed
+        private const string relativeThird = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div" +
+            "/div[3]/div/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]"; // This is the XPath of third relative Tweet in the feed
         // All are valid for now ofc
 
         public static IWebElement GetNewestTweetElement(ref WebDriver driver, WebdriverHandler.Browser browser, string profileName, ref bool exitState, string username)
@@ -19,13 +20,13 @@ namespace TwitterListener
             {
                 try
                 {
-                    IWebElement element = driver.FindElement(By.XPath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[1]/div/div/article/div/div/div/div[1]/div/div"));
+                    IWebElement element = driver.FindElement(By.XPath("/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[1]"));
                     // Get a grab of relative path of the retweet and pin data elements
                     driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0); // Remove delay for a millisec
                     if (element.FindElements(By.XPath(".//*")).Count == 0) // See if any children exist
                         return driver.FindElement(By.XPath(relativeFirst)); // This is a direct Tweet, no retweet or pin data
                     driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); // A delay of ten secs till exception is thrown when looking for element
-                    element = element.FindElement(By.XPath(".//div/div/div[2]/div/div")).FindElement(By.TagName("div")); // Crawl deeper in the HTML and close into data-testid block
+                    element = element.FindElement(By.XPath(".//div/div/div/div/div[2]/div/div")).FindElement(By.TagName("div")); // Crawl deeper in the HTML and close into data-testid block
                     // Pinned Tweets are the bane of my existence I swear to 75 varieties of a butterfly
                     if (!string.IsNullOrEmpty(element.GetAttribute("data-testid"))) // Pinned Tweets have this attribute called data-testid with socialContext in it,
                                                                                     // retweets don't have it and this will return an empty string on those
